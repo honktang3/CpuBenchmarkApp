9e801aa2a4119f3701f0d6ce1cd2f396a3db6df2
honktang3 <tang_hon_keung30@hotmail.com>
2023-08-26 22:58:35 +0800

    rectify spelling mistakes and updated readme

--- CpuBenchmarkApp/Program.cs
+++ CpuBenchmarkApp/Program.cs
@@ -22,5 +22,5 @@ TimeSpan ts = stopWatch.Elapsed;
 string elapsedTime = String.Format("{0:00}:{1:00}:{2:00}.{3:00}",
     ts.Hours, ts.Minutes, ts.Seconds,
     ts.Milliseconds / 10);
-Console.WriteLine("Total Runnign Time: " + elapsedTime);
+Console.WriteLine("Total Running Time: " + elapsedTime);
 Console.ReadKey();
--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # CpuBenchmarkApp
 This is a .NET 6 program for benchmarking your processor.
+
+Build the program in release mode and run \CpuBenchmarkApp\bin\Release\net6.0\CpuBenchmarkApp.exe to see the result.
